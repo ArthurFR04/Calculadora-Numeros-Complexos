16b933273c6ecdcbf5e7549d09918b1d00defde2
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2021-11-26 21:26:21 -0300

    1

---
+++ JS/conversor.js
@@ -0,0 +1,41 @@
+
+
+
+let Xpolar;
+let Ypolar;
+
+// Xretan;
+// Yretan;
+
+function ToRectangular(Xpolar, Ypolar) {
+    console.log(Xpolar, Ypolar);
+        let r = parseFloat(Xpolar);
+        let ang = parseFloat(Ypolar) * Math.PI / 180;
+        let x;
+        let y;
+        let Precision = 5;
+        y = r * Math.sin(ang);
+        x = r * Math.cos(ang);
+        Xretan = x.toFixed(Precision);
+        Yretan = y.toFixed(Precision);
+    
+        console.log(Xretan, Yretan);
+}
+
+function ToPolar(Xretan, Yretan) {
+
+        let x = parseFloat(Xretan);
+        let y = parseFloat(Yretan);
+        let ang;
+        let r;
+        let Precision = 5;
+        ang = Math.atan(y / x);
+        r = Math.sqrt(x * x + y * y);
+        ang2 = ang * 180 / Math.PI;
+        if (x < 0) {
+            ang2 = 180 + ang2
+        }
+        Xpolar = r.toPrecision(Precision);
+        Ypolar = ang2.toPrecision(Precision);
+
+}
